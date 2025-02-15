fc8ad863a88361d0dab29ee0fb59cf70454283d9
David Créton <davidcreton81@hotmail.fr>
2025-02-15 12:17:24 +0100

    Initial Commit Contains : Spring boot project with some config

---
+++ src/main/java/com/studio/trackflicks/TrackflicksApiApplication.java
@@ -0,0 +1,13 @@
+package com.studio.trackflicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackflicksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackflicksApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studio/trackflicks/TrackflicksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.trackflicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackflicksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
